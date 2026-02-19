6e77ab7ef4873de250c672fc970f299ef758ff56
Samiran Royy <sroy21191@gmail.com>
2026-02-19 15:36:01 +0530

    no message

--- ThreadScheduling.java
+++ ThreadScheduling.java
@@ -1,9 +1,16 @@
 public class ThreadScheduling extends Thread{
     @Override
     public void run(){
-        System.out.print("aThread is running........");
+        for(int i=0;i<=5;i++){
+            System.out.println(Thread.currentThread().getName()+ " is running........");
+            //after, Each iteration we shoukd tell  a scheduler is trying to hint a Thread to run
+            Thread.yield();
+        }
     }
     public static void main(String[] args) {
-
+    ThreadScheduling t1 = new ThreadScheduling();
+    ThreadScheduling t2 = new ThreadScheduling();
+    t1.start();
+    t2.start();
     }
 }
